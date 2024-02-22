c6062c63bb21dbf3a6c265a3c18c558deb06e190
TE22A-Assal-Yousuf <yousuf.assal@elev.ga.ntig.se>
2024-02-22 10:14:05 +0100

    start

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+int windowWidth = 1280;
+int windowHeight = 800;
+int fps = 60;
+
+Raylib.InitWindow(windowWidth, windowHeight, "IDK yet");
+Raylib.SetTargetFPS(fps);
